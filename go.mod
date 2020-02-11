6a67db259d20beeb58356557ed128ac7164c82a6
Uday Bhanu Prasad Kanagala <uday.kanagala@anaplan.com>
2020-02-11 15:45:51 -0800

    commit4

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/uday1bhanu/hello-go
 
 go 1.12
+
+require github.com/google/go-cmp v0.4.0
--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/uday1bhanu/hello-go/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
